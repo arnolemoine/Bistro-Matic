f83230979203862e2d892a251154d1d34fc12aa8
Arno Lemoine <arno.lemoine@epitech.eu>
2026-06-09 17:04:08 +0200

    [ALE](parser) : [feat] : add a big part of the parser and almost finished it

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -8,6 +8,56 @@
 #include "my.h"
 #include "bistromatic.h"
 
+num_t expr_parser(parser_t *parser)
+{
+    num_t num = term_parser(parser);
+
+    for (; parser->expr[parser->pos]; parser->pos++) {
+
+    }
+    return num;
+}
+
+num_t term_parser(parser_t *parser)
+{
+    num_t num = factor_parser(parser);
+    char o = 0;
+
+    while (parser->expr[parser->pos] == '*' || parser->expr[parser->pos] == '/' || parser->expr[parser->pos] == '%') {
+        o = parser->expr[parser->pos];
+        parser->pos++;
+        if (o == '*')
+            printf("multiplication\n");
+        if (o == '/')
+            printf("division\n");
+        if (o =='%')
+            printf("modulo\n");
+    }
+    return num;
+}
+
+num_t factor_parser(parser_t *parser)
+{
+    int sign = 1;
+    num_t num;
+
+    while (parser->expr[parser->pos] == '+' || parser->expr[parser->pos] == '-')
+        if (parser->expr[parser->pos] == '-')
+            sign *= -1;
+    if (parser->expr[parser->pos] == '(') {
+        parser->pos++;
+        num = expr_parser(parser);
+        if (parser->expr[parser->pos] != ')') {
+            my_putstr(SYNTAX_ERROR_MSG);
+            exit(EXIT_BASE);
+        }
+        parser->pos++;
+    } else
+        num = num_parser(parser);
+    num.sign = sign;
+    return num;
+}
+
 num_t num_parser(parser_t *parser)
 {
     int start = parser->pos;
@@ -19,26 +69,9 @@ num_t num_parser(parser_t *parser)
     return num;
 }
 
-num_t factor_parser(parser_t *parser)
-{
-    int sign = 1;
-    num_t num;
-
-    for (; parser->expr[parser->pos] == '+' || parser->expr[parser->pos] == '-'; parser->pos++)
-        if (parser->expr[parser->pos] == '-')
-            sign *= -1;
-    if (parser->expr[parser->pos] == '(') {
-        
-    } else {
-        num = num_parser(parser);
-    }
-    num.sign = sign;
-    return num;
-}
-
 char **parser(parser_t *parser)
 {
-    num_t num = num_parser(parser);
+    num_t num = expr_parser(parser);
 
     return NULL;
 }
