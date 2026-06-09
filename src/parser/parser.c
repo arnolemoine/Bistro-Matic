d3701db0ba23077311a20f343e8641732bb6185e
Arno Lemoine <arno.lemoine@epitech.eu>
2026-06-09 16:39:21 +0200

    [ALE](parser) : [feat] : add the begining of the parsing

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -8,22 +8,37 @@
 #include "my.h"
 #include "bistromatic.h"
 
-void parser_term(parser_t *parser, num_t *num)
+num_t num_parser(parser_t *parser)
 {
-    for(int i = parser->pos; parser->expr[i]; i++) {
-        if (parser->expr[i] == "*" || parser->expr[i] == "/") {
-            
-        }
+    int start = parser->pos;
+    num_t num;
+
+    while (is_base_char(parser->expr[parser->pos], parser->base))
+        parser->pos++;
+    num.digits = my_strndup(parser->expr + start, parser->pos - start);
+    return num;
+}
+
+num_t factor_parser(parser_t *parser)
+{
+    int sign = 1;
+    num_t num;
+
+    for (; parser->expr[parser->pos] == '+' || parser->expr[parser->pos] == '-'; parser->pos++)
+        if (parser->expr[parser->pos] == '-')
+            sign *= -1;
+    if (parser->expr[parser->pos] == '(') {
+        
+    } else {
+        num = num_parser(parser);
     }
+    num.sign = sign;
+    return num;
 }
 
 char **parser(parser_t *parser)
 {
-    num_t num;
+    num_t num = num_parser(parser);
 
-    for (; parser->expr[parser->pos]; parser->pos++) {
-        if (parser->expr[parser->pos] == "-" || parser->expr[parser->pos] == "+")
-            parser_term(parser, &num);
-    }
     return NULL;
 }
